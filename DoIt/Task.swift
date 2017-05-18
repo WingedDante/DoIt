770853c79f3dbe77d1d5940ba2948051546013b8
Logan Trecartin <logantrecartin@Logans-MacBook-Pro.local>
2017-05-18 00:45:02 -0500

    Commit with navigation controller

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Logan Trecartin on 5/17/17.
+//  Copyright © 2017 WingedDante. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    var name = ""
+    var important = false
+}
